db0556c4c230f7f2b0b3aa49719bc1dcef70a0fd
DemoRunner <DemoRunner@163.com>
2016-03-21 23:54:42 +0800

    add  the imagetodata function,which change the orignal image to the format of 'minist.pkl.gz'

--- Tools.py
+++ Tools.py
@@ -1,4 +1,10 @@
 import matplotlib.pyplot as plt
+import cPickle
+import numpy
+from PIL import Image
+from numpy import *
+import gzip
+import os
 """
 this tools is help for testing your deeplearning mould ;it can get the accuray,save the error image ,etc.
 I sincerely hope someone else can some tools in it,which are help for the beginner.
@@ -45,8 +51,47 @@ def saveimage(test_set_x, test_set_y, predicted_values):
         if test_set_y[num] != predicted_values[num]:
             image_data = test_set_x[num]
             image_lable = test_set_y[num]
-            # the original image is 28*28,you can know that:http://yann.lecun.com/exdb/mnist/
+            # the original image is 28*28,you can know
+            # that:http://yann.lecun.com/exdb/mnist/
             image = image_data.reshape((28, 28))
-            image_error = '/home/prafly/desktop/python/DeepLearning/ErrorImage/Image' + \
+            image_error = './ErrorImage/Image' + \
                 str(num) + '-' + str(image_lable) + '.png'
             plt.imsave(image_error, image, cmap=plt.cm.gray)
+
+
+def imagetodata(image_path, image_lable):
+    """ make the orignal image to data, which accord with data format of "minist.pkl.gz"
+    so all of input are Numerical image
+    :type image_path: string
+    :param image_path: the location of input image
+
+    :type image_lable: int
+    :param image_lable: the value of image
+    """
+    # Segmentation path and file name
+    if not os.path.exists(image_path):
+        return
+    else:
+        image_dir, image_f = os.path.split(image_path)
+    # take the orignal image to PIL image,and get the gray map.
+    image = Image.open(image_path).convert('L')
+    # take the PIL image to numpy arrary(the format is accord with
+    # "minist.pkl.gz" )
+    image_ndarray = numpy.asarray(image, dtype='float32') / 256
+    # the next few lines is also mean to  accord with "minist.pkl.gz"'s format.
+    image_data = image_ndarray.reshape((1, 784))
+    image_lable = array([image_lable])
+    # this line you can change,because they are same with each other.
+    # but it will not run in theano tutorials code.
+    image = ((image_data, image_lable), (image_data,
+                                         image_lable), (image_data, image_lable))
+    # use cPickle to dump image
+    write_file = open(image_dir + '/' + image_f.split(".")[0] + '.pkl', 'wb')
+    cPickle.dump(image, write_file, 0)
+
+    write_file.close()
+
+    # compress the front pkl file
+    with open(image_dir + '/' + image_f.split(".")[0] + '.pkl', 'rb') as plain_file:
+        with gzip.open(image_dir + '/' + image_f.split(".")[0] + '.pkl.gz', 'wb') as zip_file:
+            zip_file.writelines(plain_file)
